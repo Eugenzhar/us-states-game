be6cbc9b0970ce219ecf3b3c96583b76bfacb748
eugen <demontage09@rambler.ru>
2022-02-18 14:06:31 +0500

    quiz works

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import turtle
+import pandas
+from state import State
 
 screen = turtle.Screen()
+screen.setup(830, 600)
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state name?")
-print(answer_state)
+
+printed_states =[]
+game_on = True
+while game_on :
+    answer_state = screen.textinput(title="Guess the state", prompt="What's another state name?")
+    print(answer_state)
+    #print(data[data.state == answer_state])
+
+    state = (data[data.state == answer_state])
+    print(str(state.state))
+    x = int(data[data.state == answer_state].x)
+    print(x)
+    y = int(data[data.state == answer_state].y)
+    print(y)
+
+    name_state = State(answer_state, x, y)
+
+screen.mainloop()
---
+++ state.py
@@ -0,0 +1,12 @@
+from turtle import Turtle
+ALIGN ='center'
+FONT = ('Arial', 11, 'normal')
+
+class State(Turtle):
+    def __init__(self, state, x, y):
+        super().__init__()
+        self.penup()
+        self.goto(x, y)
+        self.write(f"{state}", False, align=ALIGN, font=FONT)
+        self.hideturtle()
+
